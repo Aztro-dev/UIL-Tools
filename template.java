282f393108f7960d8a61ca6a3d380f7aacdb0994
Aztro-dev <davidperezcastellanos@gmail.com>
2024-10-01 13:09:59 -0500

    Fix indentation for template.java

--- template.java
+++ template.java
@@ -4,8 +4,7 @@ import java.io.*;
 import java.math.*;
 import java.util.*;
 
-public class
-template {
+public class template {
   public static void solve(Scanner file) throws IOException {}
   public static void main(String[] args) throws IOException {
     Scanner file = new Scanner(new File("template.dat"));
