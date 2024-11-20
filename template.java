722f4912bd25105b9012a3883bda37157c2838f0
Aztro-dev <davidperezcastellanos@gmail.com>
2024-11-19 22:33:31 -0600

    Make Template uppercase

--- template.java
+++ template.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.math.*;
 import java.util.*;
 
-public class template {
+public class Template {
   public static void solve(Scanner file) throws IOException {}
   public static void main(String[] args) throws IOException {
     Scanner file = new Scanner(new File("template.dat"));
